44d2c1510a987f7ce295fb20e3c45ae7c81f2224
Renzo Crisóstomo <renzo.crisostomo@xing.com>
2016-07-31 16:34:28 +0200

    Setup basic command

--- lib/cocoapods-amimono/command/amimono.rb
+++ lib/cocoapods-amimono/command/amimono.rb
@@ -1,44 +1,18 @@
-module Pod
-  class Command
-    # This is an example of a cocoapods plugin adding a top-level subcommand
-    # to the 'pod' command.
-    #
-    # You can also create subcommands of existing or new commands. Say you
-    # wanted to add a subcommand to `list` to show newly deprecated pods,
-    # (e.g. `pod list deprecated`), there are a few things that would need
-    # to change.
-    #
-    # - move this file to `lib/pod/command/list/deprecated.rb` and update
-    #   the class to exist in the the Pod::Command::List namespace
-    # - change this class to extend from `List` instead of `Command`. This
-    #   tells the plugin system that it is a subcommand of `list`.
-    # - edit `lib/cocoapods_plugins.rb` to require this file
-    #
-    # @todo Create a PR to add your plugin to CocoaPods/cocoapods.org
-    #       in the `plugins.json` file, once your plugin is released.
-    #
-    class Amimono < Command
-      self.summary = 'Short description of cocoapods-amimono.'
+require 'claide'
+require 'cocoapods-amimono/gem_version'
 
-      self.description = <<-DESC
-        Longer description of cocoapods-amimono.
-      DESC
+module Amimono
+  class Command < CLAide::Command
 
-      self.arguments = 'NAME'
+    self.abstract_command = true
+    self.command = 'amimono'
+    self.version = CocoapodsAmimono::VERSION
+    self.description = <<-DESC
+      Move all dynamic frameworks symbols into the main executable.
+    DESC
 
-      def initialize(argv)
-        @name = argv.shift_argument
-        super
-      end
-
-      def validate!
-        super
-        help! 'A Pod name is required.' unless @name
-      end
-
-      def run
-        UI.puts "Add your implementation for the cocoapods-amimono plugin in #{__FILE__}"
-      end
+    def self.run(argv)
+      super(argv)
     end
   end
 end
