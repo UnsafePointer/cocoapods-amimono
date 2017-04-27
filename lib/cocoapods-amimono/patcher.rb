df9922b9c24491d438a1e7f123c0f051eb0b07e0
Igor Makarov <igor.makarov@moovitapp.com>
2017-04-27 09:47:24 +0300

    fix patcher so it does not strip resource bundles from vendored frameworks

--- lib/cocoapods-amimono/patcher.rb
+++ lib/cocoapods-amimono/patcher.rb
@@ -68,14 +68,8 @@ module Amimono
     # Copied over from https://github.com/CocoaPods/CocoaPods/blob/master/lib/cocoapods/installer/xcode/pods_project_generator/aggregate_target_installer.rb#L115-L131
     # with some modifications to this particular use case
     def self.resources_by_config(aggregated_target, project)
-      library_targets = aggregated_target.pod_targets.reject do |pod_target|
-        # This reject doesn't matter much anymore. We have to process all targets because
-        # every single one requires frameworks and this workaround doesn't work with Pods
-        # that contains binaries
-        !pod_target.should_build?
-      end
       aggregated_target.user_build_configurations.keys.each_with_object({}) do |config, resources_by_config|
-        resources_by_config[config] = library_targets.flat_map do |library_target|
+        resources_by_config[config] = aggregated_target.pod_targets.flat_map do |library_target|
           next [] unless library_target.include_in_build_config?(aggregated_target.target_definition, config)
           resource_paths = library_target.file_accessors.flat_map do |accessor|
             accessor.resources.flat_map { |res| res.relative_path_from(project.path.dirname) }
