e6a02cc5e80c398179c219aba3354e93df9ba38a
Vladislav Alekseev <vvalekseev@avito.ru>
2018-05-17 17:03:54 +0300

    Remove Test targets filter

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -2,9 +2,7 @@ require 'cocoapods-amimono/command'
 require 'cocoapods-amimono/build_phases_updater'
 
 Pod::HooksManager.register('cocoapods-amimono', :post_install) do |installer_context|
-  # We exclude all targets that contain `Test`, which might not work for some test targets
-  # that doesn't include that word
-  pods_targets = installer_context.umbrella_targets.reject { |target| target.cocoapods_target_label.include? 'Test' }
+  pods_targets = installer_context.umbrella_targets
   updater = Amimono::BuildPhasesUpdater.new
   updater.update_build_phases(installer_context, pods_targets)
 end
